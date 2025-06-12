a4ec11d19d67d1d137944d048cc7204bb2daac06
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-12 17:33:24 +0700

    Menambahkan iterasi tampilan elemen dari FRONT hingga akhir array (antrian melingkar)

--- Queues.cpp
+++ Queues.cpp
@@ -95,6 +95,13 @@ public:
         }
         else
         {
+             // Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << "    ";
+                FRONT_position++;
+            }
+
 
         }   
 
