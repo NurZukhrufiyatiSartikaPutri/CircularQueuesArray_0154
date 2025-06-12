8ca97844ca23c7e288fcfa2a75027cf76c30b257
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-12 17:48:58 +0700

    Menambahkan pemanggilan fungsi remove pada pilihan menu 2

--- Queues.cpp
+++ Queues.cpp
@@ -140,6 +140,11 @@ int main()
                 q.insert();
                 break;
             }
+              case '2':
+            {
+                q.remove();
+                break;
+            }
 
 
         }
