10631cc3ef3a535974ceaad27c5fcce2311c6e78
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-12 17:55:24 +0700

    Menambahkan penanganan exception untuk input yang tidak valid

--- Queues.cpp
+++ Queues.cpp
@@ -160,10 +160,11 @@ int main()
                 cout << "Invalid option!!" << endl;
                 break;
             }
-
-
-
         }
-    }    
+    } 
+    catch (exception &e)
+    {
+            cout << "Check for the values entered," << endl;
+    }   
 
 }
