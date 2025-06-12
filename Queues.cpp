0fbdde4ad3ae499fc972c61dd387b52446d7f56e
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-12 17:41:33 +0700

    Menambahkan opsi menu untuk operasi delete

--- Queues.cpp
+++ Queues.cpp
@@ -125,6 +125,7 @@ int main()
         {
             cout << "Menu" << endl;
             cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
         }
     }    
 
