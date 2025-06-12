8ef53103940dbefc15cf78776940ab7f9e22c4c2
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-12 17:11:07 +0700

    Menambahkan  pergeseran FRONT setelah penghapusan elemen

--- Queues.cpp
+++ Queues.cpp
@@ -60,6 +60,16 @@ public:
             FRONT = -1;
             REAR = -1;
         }
+         else
+        {
+            // Jika elemen yang dihapus berada diposisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+
+        }
+
         
 
     }
